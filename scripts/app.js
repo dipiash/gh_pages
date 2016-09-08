9a6ab9ab0268e1c78694fad6087d8681857e1c2c
Dm <DmitriyTemn@gmail.com>
2016-09-08 11:44:12 +0500

    Formatting

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
-$(function(){
-	window.app = new function(){
-		this.body = $('body');
-		$('#lightSwitch').on('click',$.proxy(function(){
-			this.body.toggleClass('dark');
+$(function () {
+    window.app = new function () {
+        this.body = $('body');
+        $('#lightSwitch').on('click', $.proxy(function () {
+            this.body.toggleClass('dark');
 
-			$('.ticket').each(function (index) {
-				$(this).find('.ticket__title').toggleClass('ticket__title--dark');
-				$(this).find('.ticket__message').toggleClass('ticket__message--dark');
-				$(this).find('.ticket__round-item').toggleClass('ticket__round-item--dark')
-			});
-		},this));
-	}();
+            $('.ticket').each(function (index) {
+                $(this).find('.ticket__title').toggleClass('ticket__title--dark');
+                $(this).find('.ticket__message').toggleClass('ticket__message--dark');
+                $(this).find('.ticket__round-item').toggleClass('ticket__round-item--dark')
+            });
+        }, this));
+    }();
 });
