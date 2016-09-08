d8b9a6fb9b1d37c09524a50afe450e86dc0b7022
Dm <DmitriyTemn@gmail.com>
2016-09-08 11:40:23 +0500

    Cahging theme for ticket

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,12 @@ $(function(){
 		this.body = $('body');
 		$('#lightSwitch').on('click',$.proxy(function(){
 			this.body.toggleClass('dark');
+
+			$('.ticket').each(function (index) {
+				$(this).find('.ticket__title').toggleClass('ticket__title--dark');
+				$(this).find('.ticket__message').toggleClass('ticket__message--dark');
+				$(this).find('.ticket__round-item').toggleClass('ticket__round-item--dark')
+			});
 		},this));
 	}();
 });
