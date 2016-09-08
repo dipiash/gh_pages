e37afb39db52a6cb62c719914283bb4451fd0f60
Dm <DmitriyTemn@gmail.com>
2016-09-08 12:01:07 +0500

    Field for all tickets

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ $(function () {
             .clone()
             .removeClass('ticket__clone');
 
+        var ticketsLine = $('#tickets-line');
+
         this.body = $('body');
         $('#lightSwitch').on('click', $.proxy(function () {
             this.body.toggleClass('dark');
